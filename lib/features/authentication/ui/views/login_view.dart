7d0f42b98f907ce3fc5b819d6605886d4ca27f8c
Ahmed Taha <101575320+ahmed-taha1@users.noreply.github.com>
2024-02-16 01:37:52 +0200

    login view created

---
+++ lib/features/authentication/ui/views/login_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('LoginView'),
+      ),
+    );
+    // return const LoginViewBody();
+  }
+}
