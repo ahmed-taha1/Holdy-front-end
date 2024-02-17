9b57fd2484ad508422aa400ceb7815fd9eddbd7a
Ahmed Taha <101575320+ahmed-taha1@users.noreply.github.com>
2024-02-17 15:14:03 +0200

    app name changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'accounts_protector_app.dart';
 
 
-void main() {
+void main() async{
+  // setupGetIT();
+  // to fix .sp font bug in flutter screentil in release mode
+  await ScreenUtil.ensureScreenSize();
   runApp(const AccountsProtectorApp());
 }
