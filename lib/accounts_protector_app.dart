6fcb537bd74855963ad498b34533cdf1333b7c52
ahmedhany1284 <ahmedhany4900@gmail.com>
2024-03-12 01:00:05 +0200

    added bloc builder and provider to the log in and the main

--- lib/accounts_protector_app.dart
+++ lib/accounts_protector_app.dart
@@ -1,6 +1,8 @@
 import 'package:accounts_protector/core/theming/app_colors.dart';
 import 'package:accounts_protector/core/routing/app_router.dart';
+import 'package:accounts_protector/features/authentication/logic/login_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/helper/font_weight_helper.dart';
@@ -14,18 +16,23 @@ class AccountsProtectorApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
-      child: MaterialApp.router(
-        routerConfig: AppRouter.router,
-        debugShowCheckedModeBanner: false,
-        title: 'Accounts Protector',
-        theme: ThemeData(
-          fontFamily: FontWeightHelper.regular,
-          scaffoldBackgroundColor: AppColors.white,
-          primaryColor: AppColors.blackPurple,
-          primarySwatch: Colors.grey,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-          useMaterial3: true,
-          textTheme: ThemeData.light().textTheme,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => LoginCubit()),
+        ],
+        child: MaterialApp.router(
+          routerConfig: AppRouter.router,
+          debugShowCheckedModeBanner: false,
+          title: 'Accounts Protector',
+          theme: ThemeData(
+            fontFamily: FontWeightHelper.regular,
+            scaffoldBackgroundColor: AppColors.white,
+            primaryColor: AppColors.blackPurple,
+            primarySwatch: Colors.grey,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            useMaterial3: true,
+            textTheme: ThemeData.light().textTheme,
+          ),
         ),
       ),
     );
--- lib/features/authentication/ui/widgets/login_widgets/login_view_body.dart
+++ lib/features/authentication/ui/widgets/login_widgets/login_view_body.dart
@@ -19,85 +19,91 @@ class LoginViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
-    return SizedBox(
-      height: double.infinity,
-      child: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        padding: EdgeInsets.symmetric(vertical: 5.h),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.w),
-              child: Column(
-                children: [
-                  verticalSpace(85),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10.w),
-                    child: const DefaultAuthText(
-                      mainText: 'Login',
-                      subText: 'Please sign in to continue',
-                      isLogoIncluded: true,
-                    ),
-                  ),
-                  verticalSpace(33),
-                  CustomInputField(
-                    controller: emailController,
-                    hintText: 'EMAIL',
-                    icon: Icons.email_outlined,
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  verticalSpace(12),
-                  CustomInputField(
-                    controller: passwordController,
-                    hintText: 'PASSWORD',
-                    icon: Icons.lock,
-                    keyboardType: TextInputType.visiblePassword,
-                    isPassword: true,
-                  ),
-                ],
-              ),
-            ),
-            verticalSpace(18),
-            CustomButton(
-              onPressed: () async{
-                await context.read<LoginCubit>().login(
-                  email: emailController.text,
-                  password: passwordController.text,
-                );
-                if (context.read<LoginCubit>().state is LoginSuccessState) {
-                  context.go(Routes.platformsView.path);
-                }
-              },
-              text: 'LOGIN',
-            ),
-            verticalSpace(14),
-            CustomTextButton(
-              text: 'Forgot Password?',
-              textStyle: TextStyles.font14SemiLightBlueBold,
-              onPressed: () {
-                context.push(Routes.forgotPasswordView.path);
-              },
-            ),
-            verticalSpace(50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return BlocBuilder<LoginCubit, LoginStates>(
+      builder: (context, state) {
+        return SizedBox(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            padding: EdgeInsets.symmetric(vertical: 5.h),
+            child: Column(
               children: [
-                Text(
-                  "Don't have an account?",
-                  style: TextStyles.font15DarkGreySemiBold,
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.w),
+                  child: Column(
+                    children: [
+                      verticalSpace(85),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.w),
+                        child: const DefaultAuthText(
+                          mainText: 'Login',
+                          subText: 'Please sign in to continue',
+                          isLogoIncluded: true,
+                        ),
+                      ),
+                      verticalSpace(33),
+                      CustomInputField(
+                        controller: emailController,
+                        hintText: 'EMAIL',
+                        icon: Icons.email_outlined,
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      verticalSpace(12),
+                      CustomInputField(
+                        controller: passwordController,
+                        hintText: 'PASSWORD',
+                        icon: Icons.lock,
+                        keyboardType: TextInputType.visiblePassword,
+                        isPassword: true,
+                      ),
+                    ],
+                  ),
                 ),
+                verticalSpace(18),
+                CustomButton(
+                  onPressed: () async {
+                    await context.read<LoginCubit>().login(
+                      email: emailController.text,
+                      password: passwordController.text,
+                    );
+                    if (context
+                        .read<LoginCubit>()
+                        .state is LoginSuccessState) {
+                      context.go(Routes.platformsView.path);
+                    }
+                  },
+                  text: 'LOGIN',
+                ),
+                verticalSpace(14),
                 CustomTextButton(
-                  text: " Register",
+                  text: 'Forgot Password?',
                   textStyle: TextStyles.font14SemiLightBlueBold,
                   onPressed: () {
-                    context.push(Routes.registerView.path);
+                    context.push(Routes.forgotPasswordView.path);
                   },
+                ),
+                verticalSpace(50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyles.font15DarkGreySemiBold,
+                    ),
+                    CustomTextButton(
+                      text: " Register",
+                      textStyle: TextStyles.font14SemiLightBlueBold,
+                      onPressed: () {
+                        context.push(Routes.registerView.path);
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
